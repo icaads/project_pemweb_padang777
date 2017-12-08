48c2e88cba1662a01661a8818add8a83c8746922
icaads <veronica.sari@student.umn.ac.id>
2017-12-08 17:18:05 +0700

    coba

--- application/controllers/Reservasi.php
+++ application/controllers/Reservasi.php
@@ -7,7 +7,8 @@ class Reservasi extends CI_Controller{
 		parent::__construct();
 		
 		$this->load->model('Cabang');
-		$this->load->model('Meja');
+		$this->load->model('Meja'); 
+		$this->load->model('Reservasis');
 	}
 
 	public function index(){
@@ -21,6 +22,8 @@ class Reservasi extends CI_Controller{
 		$data['navbar_guest'] = $this->load->view('include/navbar_guest.php',NULL,TRUE);
 		$data['footer'] = $this->load->view('include/footer.php',NULL,TRUE);
 		$data['button_script'] = $this->load->view('include/button_script.php',NULL,TRUE);
+		$data['modal_picture'] = $this->load->view('include/modal_picture.php',NULL,TRUE);
+		$data['modalcss'] = $this->load->view('include/modalcss.php',NULL,TRUE);
 		$this->load->view('pages/reservationform.php',$data);
 	}
 
@@ -32,21 +35,17 @@ class Reservasi extends CI_Controller{
 	}
 
 	function pesan(){
-		
+		$tanggal = $this->input->post('waktu_datang');
+		$jumlah = $this->input->post('jumlah_tamu');
+		$meja = $this->input->post('pilih_meja');
+		$data = array(
+			'IDMeja'=> $meja,
+			'tanggalreservasi' => $tanggal,
+			'jumlah_tamu' => $jumlah
+		);
+			
+		var_dump($data);
+		$this->Reservasis->input($data);
 	}
 
-	function denah(){
-		$idlokasi = $this->session->userdata('lokasi');
-		
-		$data['meja'] = $this->Meja->showData($idlokasi);
-		//return var_dump($data['Cabang']);
-		$data['js'] = $this->load->view('include/js.php',NULL,TRUE);
-		$data['css'] = $this->load->view('include/css.php',NULL,TRUE);
-		$data['navigation_login'] = $this->load->view('include/navigation_login.php',NULL,TRUE);
-		$data['navbar_guest'] = $this->load->view('include/navbar_guest.php',NULL,TRUE);
-		$data['footer'] = $this->load->view('include/footer.php',NULL,TRUE);
-		$data['button_script'] = $this->load->view('include/button_script.php',NULL,TRUE);
-		$this->load->view('pages/reservationform.php',$data);
-		//var_dump($idlokasi);
-	}
 }
--- application/views/pages/reservationform.php
+++ application/views/pages/reservationform.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-	<?php echo $js; echo $css;?>
+	<?php echo $js;echo $modalcss; echo $css;  ?>
 	<div class="container">
 		<?php
 			if($this->session->userdata('status')=='member'){
@@ -18,11 +18,12 @@
 			}
 			
 		?>
+		
 			<?php echo form_open('reservasi/pesan'); ?>
 			<br>
 			<br>
 			<br>
-		
+
 			<h2 class="text-center">Formulir Pemesanan Meja</h2>
 			<br>
 			<br>
@@ -52,24 +53,19 @@
 						<div class="form-group">
 							<label for="jumlah_tamu" class="col-sm-3 control-label">Jumlah Tamu</label>
 							<div class="col-sm-12">
-								
-									
-									
-                                        <div class="input-group">
-                                    <span class="input-group-btn">
-                                        <button type="button" class="quantity-left-minus btn btn-info btn-number"  data-type="minus" data-field="">
-                                          <span class="fa fa-minus"></span>
-                                        </button>
-                                    </span>
+								<div class="input-group">
+									<span class="input-group-btn">
+										<button type="button" class="quantity-left-minus btn btn-info btn-number" data-type="minus" data-field="">
+											<span class="fa fa-minus"></span>
+										</button>
+									</span>
 									<input type="text" id="quantity" name="quantity" class="form-control input-number text-center" value="1" min="1" max="100">
-                                    <span class="input-group-btn">
-                                        <button type="button" class="quantity-right-plus btn btn-info btn-number" data-type="plus" data-field="">
-                                            <span class="fa fa-plus"></span>
-                                        </button>
-                                    </span>
-                                </div>
-                        
-								
+									<span class="input-group-btn">
+										<button type="button" class="quantity-right-plus btn btn-info btn-number" data-type="plus" data-field="">
+											<span class="fa fa-plus"></span>
+										</button>
+									</span>
+								</div>
 							</div>
 						</div>
 						<div class="form-group">
@@ -79,47 +75,47 @@
 									<div class="input-group-addon bg-light">
 										<i class="fa fa-spoon text-info"></i>
 									</div>
-									
-									
 									<select multiple class="form-control">
-									<?php foreach($meja as $row){ ?>
-									<option>
-										<?php echo $row['LokasiMeja'];?>
-									</option>
-									<?php } ?>
-								  </select>
-								  
-									
+										<?php foreach($meja as $row){ ?>
+										<option>
+											<?php echo $row['LokasiMeja'];?>
+										</option>
+										<?php } ?>
+									</select>
 								</div>
-								</div>
-								<label for="pilih_meja" class="col-sm-12 control-label">Tekan ctrl untuk memilih lebih dari 1</label>
 							</div>
-							
-						
-
+							<label for="pilih_meja" class="col-sm-12 control-label">Tekan ctrl untuk memilih lebih dari 1</label>
+						</div>
 						<div class="text-center">
 							<button type="submit" class="btn btn-primary btn-xs" style="width:40%">Submit</button>
 						</div>
 						<br>
-
 					</div>
 					<br>
 					<br>
-								<?php echo form_close(); ?>
-
-
+					<?php echo form_close(); ?>
 				</div>
 				<div>
-				
-					<h2 class ="text-center">Denah Lokasi Meja</h2>
-						<?php
+					<h2 class="text-center">Denah Lokasi Meja</h2>
+					<?php
 						if($Cabang != NULL){ ?>
-							<img src=" <?php echo base_url('');?><?php echo $Cabang[0]['LinkDenah']?>" style="width:550px;height:400px">
+						<img id="gambar" src=" <?php echo base_url('');?><?php echo $Cabang[0]['LinkDenah']?>" style="width:500px;height:400px;">
 						<?php }
 						?>
 						
+						<div id="myModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="denah">
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption">apa</div>
+</div>
 				</div>
-						
+
 			</div>
 
 
@@ -127,7 +123,7 @@
 	<br>
 	<br>
 
-	<?php echo $footer; echo $button_script;?>
+	<?php echo $footer; echo $button_script; echo $modal_picture;?>
 </body>
 
 </html>
