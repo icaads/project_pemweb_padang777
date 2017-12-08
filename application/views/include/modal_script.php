aff7e037a465c70d7ab174d6351d8a8d8c098766
jennifergbrlle <jennifer.nangoy@student.umn.ac.id>
2017-12-08 18:13:21 +0700

    l

--- application/views/include/modal_script.php
+++ application/views/include/modal_script.php
@@ -1,23 +1,13 @@
 <script type="text/javascript">
 			
 			$(document).ready(function(){
-				$('#signin').modal({
+				$('#modal_guest').modal({
 					keyboard: false,
 					show: 'fade',
 					backdrop: 'static',
 				});
 			});
 		</script>
-
->
-
-<script type="text/javascript">
-    $(window).on('load',function(){
-        $('#modal_guest').modal({backdrop: 'static', keyboard: false})
-		
-    });
-</script>
-
 
 <script type="text/javascript">
     $(window).on('load',function(){
