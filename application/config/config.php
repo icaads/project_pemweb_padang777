b197d60ac98430c5887e1a00f9563fc96c44dc9b
jennifergbrlle <jennifer.nangoy@student.umn.ac.id>
2017-12-08 12:56:21 +0700

    a

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/padang777';
+$config['base_url'] = 'http://localhost:8080/project_pemweb_padang777';
 
 /*
 |--------------------------------------------------------------------------
