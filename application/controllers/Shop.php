d65591d61b5d53b460408c4da4d7b792d7938cae
jennifergbrlle <jennifer.nangoy@student.umn.ac.id>
2017-12-09 14:04:33 +0700

    l

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -8,6 +8,7 @@ class Shop extends CI_Controller{
 		$data['js'] = $this->load->view('include/js.php',NULL,TRUE);
 		$data['css'] = $this->load->view('include/css.php',NULL,TRUE);
 		$data['navigation_login'] = $this->load->view('include/navigation_login.php',NULL,TRUE);
+		$data['navbar_guest'] = $this->load->view('include/navbar_guest.php',NULL,TRUE);
 		$data['footer'] = $this->load->view('include/footer.php',NULL,TRUE);
 		$data['modal_script'] = $this->load->view('include/modal_script.php',NULL,TRUE);
 		$data['menu'] = $this->menu->show_menu();
@@ -16,10 +17,13 @@ class Shop extends CI_Controller{
 
 	public function kategori_menu(){
 		$this->load->model('menu');
-		$menu['data'] = $this->menu->kategori_menu();
+		$asd = $this->input->get('id');
+		var_dump("$asd");
+		$data['menu'] = $this->menu->kategori_data($asd);
 		$data['js'] = $this->load->view('include/js.php',NULL,TRUE);
 		$data['css'] = $this->load->view('include/css.php',NULL,TRUE);
 		$data['navbar_guest'] = $this->load->view('include/navbar_guest.php',NULL,TRUE);
+		$data['navigation_login'] = $this->load->view('include/navigation_login.php',NULL,TRUE);
 		$data['footer'] = $this->load->view('include/footer.php',NULL,TRUE);
 		$data['modal_script'] = $this->load->view('include/modal_script.php',NULL,TRUE);
 		//var_dump($data);
@@ -28,7 +32,7 @@ class Shop extends CI_Controller{
 		//$this->menu->show($isi);
 		
 
-		$this->load->view('pages/shop_guest.php',$data);
+		$this->load->view('pages/shop.php',$data);
 	}
 }
 ?>
